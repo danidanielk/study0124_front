5bd622a50a28a3290c90de8e1bac124dc99c1c78
daniel <danielk0331@naver.com>
2023-01-27 00:41:07 +0900

    '0127'

--- src/routes/Join.js
+++ src/routes/Join.js
@@ -44,76 +44,85 @@ function Join() {
       })
       .catch((error) => {
         console.log(error);
+        alert("id 중복 & pw 확인");
       });
   };
+
+  // .catch((error) => {
+  //   console.log(error);
+  // });
+
   return (
-    <div class="mx-auto flex min-h-screen w-full bg-gray-900 p-4 flex items-center justify-center">
-      <div class="bg-gray-900 py-6 px-10 sm:max-w-md w-full ">
-        <div class="sm:text-3xl text-2xl font-semibold text-center text-[#FF6A3D]  mb-12">
-          Registration Form
+    <div className="mx-auto flex min-h-screen w-full bg-gray-900 p-4 flex items-center justify-center">
+      <div className="bg-gray-900 py-6 px-10 sm:max-w-md w-full ">
+        <div className="sm:text-3xl text-2xl font-semibold text-center text-[#FF6A3D]  mb-12">
+          Registration
         </div>
-        <div class="text-white">
-          <div class="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
+        <div className="text-white">
+          <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
             <input
               type="text"
               placeholder="Name"
-              class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+              className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
               value={name}
               onChange={onName}
             />
           </div>
-          <div class="w-full mt-8 transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
+
+          <div className="w-full mt-8 transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
             <input
               type="email"
               placeholder="Email address"
-              class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+              className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
               value={email}
               onChange={onEmail}
             />
           </div>
-          <div class="w-full mt-8 transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
+          <div className="w-full mt-8 transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
             <input
               type="text"
               placeholder="Phone"
-              class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+              className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
               value={phone}
               onChange={onPhone}
             />
           </div>
-          <div class="w-full mt-8 transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
+          <div className="w-full mt-8 transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
             <input
               type="password"
               placeholder="Password"
-              class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+              className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
               value={password}
               onChange={onPassword}
             />
           </div>
-          <div class="w-full mt-8 transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
+          <div className="w-full mt-8 transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
             <input
               type="password"
               placeholder="Password"
-              class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+              className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
               value={password2}
               onChange={onPassword2}
             />
           </div>
 
-          <div class="flex">
-            <input type="checkbox" class="border-[#FF6A3D] " value="" />
-            <div class="px-3 text-gray-500">I accept terms & conditions</div>
+          <div className="flex">
+            <input type="checkbox" className="border-[#FF6A3D] " value="" />
+            <div className="px-3 text-gray-500">
+              I accept terms & conditions
+            </div>
           </div>
-          <div class="flex justify-center my-6">
+          <div className="flex justify-center my-6">
             <button
-              class=" rounded-full  p-3 w-full sm:w-56   bg-[#FF6A3D] text-white text-lg font-semibold "
+              className=" rounded-full  p-3 w-full sm:w-56   bg-[#FF6A3D] text-white text-lg font-semibold "
               onClick={onJoin}
             >
               Create Account
             </button>
           </div>
-          <div class="flex justify-center ">
-            <p class="text-gray-500">Already have an acount? </p>
-            <button href="" class="text-sky-600 pl-2" onClick={onLogin}>
+          <div className="flex justify-center ">
+            <p className="text-gray-500">Already have an acount? </p>
+            <button href="" className="text-sky-600 pl-2" onClick={onLogin}>
               Sign In
             </button>
           </div>
--- src/routes/Login.js
+++ src/routes/Login.js
@@ -26,41 +26,43 @@ function Login() {
       })
       .then((response) => {
         console.log(response);
+        window.location.assign("/");
       })
+
       .catch((error) => {
+        alert("id,pw 확인");
         console.log(error);
       });
-    window.location.assign("/");
   };
 
   return (
     <>
-      <main class="mx-auto flex min-h-screen w-full items-center justify-center bg-gray-900 text-white">
-        <section class="flex w-[30rem] flex-col space-y-10">
-          <div class="text-center text-4xl font-medium">Log In</div>
+      <main className="mx-auto flex min-h-screen w-full items-center justify-center bg-gray-900 text-white">
+        <section className="flex w-[30rem] flex-col space-y-10">
+          <div className="text-center text-4xl font-medium">Log In</div>
 
-          <div class="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
+          <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
             <input
               type="text"
               placeholder="Email or Username"
-              class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+              className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
               value={email}
               onChange={onEmail}
             />
           </div>
 
-          <div class="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
+          <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
             <input
               type="password"
               placeholder="Password"
-              class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+              className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
               value={password}
               onChange={onPassword}
             />
           </div>
 
           <button
-            class="transform rounded-sm bg-[#FF6A3D] py-2 font-bold duration-300 hover:bg-indigo-400"
+            className="transform rounded-sm bg-[#FF6A3D] py-2 font-bold duration-300 hover:bg-indigo-400"
             onClick={onLogin}
           >
             LOG IN
@@ -68,16 +70,16 @@ function Login() {
 
           <button
             href="#"
-            class="transform text-center font-semibold text-gray-500 duration-300 hover:text-gray-300"
+            className="transform text-center font-semibold text-gray-500 duration-300 hover:text-gray-300"
           >
             FORGOT PASSWORD?
           </button>
 
-          <p class="text-center text-lg">
+          <p className="text-center text-lg">
             No account?
             <button
               href="#"
-              class="font-medium text-[#FF6A3D] underline-offset-4 hover:underline"
+              className="font-medium text-[#FF6A3D] underline-offset-4 hover:underline"
               onClick={onJoin}
             >
               Create One
